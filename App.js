bad3ceed479fef29ea48781cbdf8674e509d6ece
jleonard17 <71112878+jleonard17@users.noreply.github.com>
2021-01-24 12:32:07 -0500

    Tesla clone

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +19,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
